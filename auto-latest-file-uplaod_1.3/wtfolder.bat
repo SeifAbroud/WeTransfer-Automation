9139f94bc4b398151310b5fb2fcce3f6f6db03d8
SeifAbroud <132721776+SeifAbroud@users.noreply.github.com>
2024-02-01 16:49:05 +0100

    auto-selecting and uploading the most recent file in a given foder. - Introduced a new version of the upload function . - Users can create a folder to store files for upload . - The code now automatically selects the most recent file in the folder for upload - Added a command line option 'wtfolder' to trigger the upload process

--- auto-latest-file-uplaod_1.3/wtfolder.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-py C:\Users\abrou\PycharmProjects\pythonProject7\Wetransfere-automation\wt-folder-auto-upload.py
---
+++ auto-latest-file-upload_1.3/wtfolder.bat
@@ -0,0 +1,2 @@
+@echo off
+py C:\Users\abrou\PycharmProjects\pythonProject7\Wetransfere-automation\wt-folder-auto-upload.py
